cf0b5256c49fe7cf29caefc7df7479226e3e13f3
ahmedaefattah <ahmed.aefattah@gmail.com>
2022-01-25 16:49:33 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxQrcodeElementTypes} from '@techiediaries/ngx-qrcode';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qr-code-generator';
+  elementType = NgxQrcodeElementTypes.URL;
+  value = 'www.example.com';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxQRCodeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
